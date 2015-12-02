c961d2265cf05b7d431576b411649da8cf9d2099
JakeAlewel <jakealewel@ymail.com>
2015-12-01 19:25:02 -0800

    first commit

---
+++ NSWindowShopper/ProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewController.swift
+//  NSWindowShopper
+//
+//  Created by iGuest on 12/1/15.
+//  Copyright © 2015 iGuest. All rights reserved.
+//
+
+import Foundation
---
+++ NSWindowShopper/SearchSettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchSettingsViewController.swift
+//  NSWindowShopper
+//
+//  Created by iGuest on 12/1/15.
+//  Copyright © 2015 iGuest. All rights reserved.
+//
+
+import Foundation
