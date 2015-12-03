8f958e8f753fca50de4aedd63aa9199a7dbb0cb5
AlexSchenck <AlexNSchenck@gmail.com>
2015-12-02 23:36:39 -0800

    Initial Item detail view

--- NSWindowShopper/ItemDetailViewController.swift
+++ NSWindowShopper/ItemDetailViewController.swift
@@ -8,14 +8,40 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
 class ItemDetailViewController : UIViewController {
+    
+    @IBOutlet weak var ItemImage: UIImageView!
+    @IBOutlet weak var ItemName: UILabel!
+    @IBOutlet weak var ItemLocation: UILabel!
+    @IBOutlet weak var DatePosted: UILabel!
+    @IBOutlet weak var ItemDescription: UILabel!
+    
+    var sampleItem = Item(profile: Profile(), name: "Football", description: "Boy howdy this football sure is golly gee swell you should buy the hell out of it", price: NSNumber(int: 20), location: CLLocation(), locationName: "Seattle, WA", datePosted: NSDate(), imageURL: "https://beccasheppard.files.wordpress.com/2011/09/football.jpg")
+    
+    override func viewDidLoad() {
+        if let url = NSURL(string: sampleItem.imageURL!) {
+            if let data = NSData(contentsOfURL: url) {
+                ItemImage.image = UIImage(data: data)
+            }
+        }
+        
+        ItemImage.clipsToBounds = true
+        
+        ItemName.text = "\(sampleItem.name!), $\(sampleItem.price!.stringValue)"
+        ItemLocation.text = sampleItem.locationName!
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        DatePosted.text = "Posted: \(dateFormatter.stringFromDate(sampleItem.datePosted!))"
+        
+        ItemDescription.text = sampleItem.description
+    }
     
     @IBAction func navigateToProfileViewController(sender: AnyObject) {
         let storyboard = UIStoryboard(name: "ProfileViewController", bundle: nil)
         let vc = storyboard.instantiateViewControllerWithIdentifier("ProfileViewController") as UIViewController
         self.navigationController?.pushViewController(vc, animated: true)
-        
-        
     }
 }
