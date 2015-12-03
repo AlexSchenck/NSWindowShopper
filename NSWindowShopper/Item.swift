927c589971694f4e7936c555b6ae390ba0a5fec3
Alex Schenck <AlexNSchenck@gmail.com>
2015-12-02 19:34:34 -0800

    Added init for Item

--- NSWindowShopper/Item.swift
+++ NSWindowShopper/Item.swift
@@ -11,13 +11,26 @@ import CoreLocation
 
 class Item {
  
-    var profile : Profile?;
-    var name : String?;
-    var description : String?;
-    var price : NSNumber?;
-    var location : CLLocation?;
-    var locationName : String?;
-    var datePosted : NSDate?;
-    var imageURL : String?;
-    
+    var profile : Profile?
+    var name : String?
+    var description : String?
+    var price : NSNumber?
+    var location : CLLocation?
+    var locationName : String?
+    var datePosted : NSDate?
+    var imageURL : String?
+ 
+    init(profile : Profile, name : String, description : String,
+        price : NSNumber, location : CLLocation, locationName : String,
+        datePosted : NSDate, imageURL : String) {
+        
+        self.profile = profile
+        self.name = name
+        self.description = description
+        self.price = price
+        self.location = location
+        self.locationName = locationName
+        self.datePosted = datePosted
+        self.imageURL = imageURL
+    }
 }
