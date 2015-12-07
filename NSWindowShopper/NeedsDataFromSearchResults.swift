a16256b6df7449b262606a873d9bbaeae87f4e36
Jacob Alewel <jakealewel@ymail.com>
2015-12-06 20:47:44 -0800

    Added new view controllers for the three tabs

---
+++ NSWindowShopper/NeedsDataFromSearchResults.swift
@@ -0,0 +1,9 @@
+//
+//  NeedsDataFromSearchResults.swift
+//  NSWindowShopper
+//
+//  Created by Jacob Alewel on 12/6/15.
+//  Copyright © 2015 iGuest. All rights reserved.
+//
+
+import Foundation
